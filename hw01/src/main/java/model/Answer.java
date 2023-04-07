04e0cad56a360ad6a079634a52a3b9cb44986322
Egor Shevko <egorshevko@gmail.com>
2023-04-07 18:36:57 +0300

    init commit

---
+++ hw01/src/main/java/model/Answer.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Answer {
+    private String value;
+
+    private Integer index;
+
+    public Answer(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
